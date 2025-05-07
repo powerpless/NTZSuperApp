a673af92ad9cf6601aa84c452e6c65d0eff27ff0
Tima <8vdead@gmail.com>
2025-05-07 16:09:02 +0500

    Initial commit

---
+++ src/main/java/org/example/ntzsuperapp/NtzSuperAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.ntzsuperapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtzSuperAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtzSuperAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ntzsuperapp/NtzSuperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ntzsuperapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtzSuperAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
